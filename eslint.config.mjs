37228fa242bf40de5ca21de06483e9e6c392604d
hiem55 <oh.justin55@gmail.com>
2024-05-01 15:30:32 -0700

    setup eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,13 @@
 import { FlatCompat } from '@eslint/eslintrc';
-import pluginReact from 'eslint-plugin-react';
-import pluginReactHooks from 'eslint-plugin-react-hooks';
-import pluginNext from 'eslint-plugin-next';
+import path from 'path'
+import { fileURLToPath } from 'url'
+import pluginJs from '@eslint/js'
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat();
+const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended })
 
 export default [
     {
@@ -18,7 +18,7 @@ export default [
                 process: 'writable',  // Node.js process
             },
         },
-        ignorePatterns: ['*.py', 'node_modules/'],  // Ignore Python files
+        ignores: ['*.py', 'node_modules/'],  // Ignore Python files
 
         rules: {
             'semi': ['error', 'always'],  // Require semicolons
@@ -26,15 +26,6 @@ export default [
             'indent': ['error', 2],  // Enforce 2-space indentation
         },
     },
-    {
-        plugins: [pluginReact, pluginReactHooks, pluginNext],  // React, React Hooks, and Next.js plugins
-        rules: {
-            'react/jsx-uses-react': 'error',  // Ensure React components are used
-            'react/jsx-uses-vars': 'error',  // Prevent unused variables in JSX
-            'react-hooks/rules-of-hooks': 'error',  // Enforce React Hooks rules
-            'react-hooks/exhaustive-deps': 'warn',  // Check hook dependency arrays
-            'next/no-html-link-for-pages': 'error',  // Enforce Next.js conventions
-        },
-    },
+    ...compat.extends('eslint:recommended'),  // Use recommended rules (ESLint 8+
     eslintConfigPrettier,  // Ensure compatibility with Prettier
 ];
