36e446016be7610ac9ddbd60c74e856fc9c5e623
hiem55 <oh.justin55@gmail.com>
2024-05-01 15:02:53 -0700

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+        es6: true
+    },
+    extends: ['eslint:recommended'],
+    rules: {
+        'no-unused-vars': ['error'],
+        'semi': ['error', 'always'],
+        'quotes': ['error', 'double'],
+        'indent': ['error', 2]
+    }
+};
